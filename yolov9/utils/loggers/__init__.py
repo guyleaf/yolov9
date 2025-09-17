7b21bf4472fbe44784196a3d78ceaa4f6df3d99f
Leaf Ying <leaf.ying.work@gmail.com>
2025-09-17 21:56:12 +0800

    fix: Remove unnecessary login function call in wandb to work in offline mode

--- yolov9/utils/loggers/__init__.py
+++ yolov9/utils/loggers/__init__.py
@@ -18,13 +18,8 @@ try:
     import wandb
 
     assert hasattr(wandb, '__version__')  # verify package import not local dir
-    if pkg.parse_version(wandb.__version__) >= pkg.parse_version('0.12.2') and RANK in {0, -1}:
-        try:
-            wandb_login_success = wandb.login(timeout=30)
-        except wandb.errors.UsageError:  # known non-TTY terminal issue
-            wandb_login_success = False
-        if not wandb_login_success:
-            wandb = None
+    if not pkg.parse_version(wandb.__version__) >= pkg.parse_version('0.12.2'):
+        wandb = None
 except (ImportError, AssertionError):
     wandb = None
 
