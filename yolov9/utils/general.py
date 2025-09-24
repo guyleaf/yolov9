22fe7e34b0c19c555a3f0efd6b664cec710017db
Leaf Ying <leaf.ying.work@gmail.com>
2025-09-24 15:45:24 +0800

    feat: Support customizing the behavior of loading weights

--- yolov9/utils/general.py
+++ yolov9/utils/general.py
@@ -253,6 +253,8 @@ def intersect_dicts(da, db, exclude=()):
     # Dictionary intersection of matching keys and shapes, omitting 'exclude' keys, using da values
     return {k: v for k, v in da.items() if k in db and all(x not in k for x in exclude) and v.shape == db[k].shape}
 
+def exclude_dict(d, exclude=()):
+    return {k: v for k, v in d.items() if all(x not in k for x in exclude)}
 
 def get_default_args(func):
     # Get func() default arguments
