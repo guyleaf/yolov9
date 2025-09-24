b1a0074788c3a37cd9739e3d349b3e9b29be010c
Leaf Ying <leaf.ying.work@gmail.com>
2025-09-24 16:41:55 +0800

    fix: make DATASETS_DIR variable compatible with original behavior

--- yolov9/utils/general.py
+++ yolov9/utils/general.py
@@ -98,7 +98,7 @@ def is_writeable(dir, test=False):
 
 # Settings
 NUM_THREADS = min(8, max(1, os.cpu_count() - 1))  # number of YOLOv5 multiprocessing threads
-DATASETS_DIR = Path(os.getenv('YOLOv5_DATASETS_DIR', WORKDIR_ROOT.parent if GIT_ROOT and is_writeable(WORKDIR_ROOT.parent) else WORKDIR_ROOT / 'datasets'))  # global datasets directory
+DATASETS_DIR = Path(os.getenv('YOLOv5_DATASETS_DIR', WORKDIR_ROOT / 'datasets'))  # global datasets directory
 AUTOINSTALL = str(os.getenv('YOLOv5_AUTOINSTALL', True)).lower() == 'true'  # global auto-install mode
 VERBOSE = str(os.getenv('YOLOv5_VERBOSE', True)).lower() == 'true'  # global verbose mode
 TQDM_BAR_FORMAT = '{l_bar}{bar:10}| {n_fmt}/{total_fmt} {elapsed}'  # tqdm bar format
